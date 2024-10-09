9ed3a46b4be595141ad3167bd14a1f6a9644dad6
JiaWei-Chen-2295 <1601020332@qq.com>
2024-10-09 18:06:11 +0800

    DAY-10-初识Vue_CLI

---
+++ 04-单文件组件/X.vue
@@ -0,0 +1,21 @@
+<!-- 结构 -->
+<template>
+    <div>
+        <h2>
+            输入数字 <input type="text">
+        </h2>
+        <XX></XX>
+    </div>
+</template>
+
+<!-- 交互 -->
+<script>
+    import XX from "./XX.vue"
+    const x = {
+        component : {XX}
+    }
+
+    // 默认导出
+    export default x
+</script>
+
